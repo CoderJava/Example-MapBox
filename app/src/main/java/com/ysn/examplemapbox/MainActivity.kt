bba7c5408bbc09fbf325db793967b8fe35d3b09f
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-02-24 01:07:42 +0700

    Added sample showing device location and add marker with click listener on map
    Added sample showing device location and add marker with click listener on map

--- app/src/main/java/com/ysn/examplemapbox/MainActivity.kt
+++ app/src/main/java/com/ysn/examplemapbox/MainActivity.kt
@@ -1,26 +1,89 @@
 package com.ysn.examplemapbox
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.annotations.MarkerOptions
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MapboxMap.OnMapClickListener {
 
     lateinit var mapView: MapView
+    lateinit var mapBoxMap: MapboxMap
+    lateinit var markerViewManager: MarkerViewManager
+    lateinit var permissionsManager: PermissionsManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(this, getString(R.string.access_token))
         setContentView(R.layout.activity_main)
+        initMapBox(savedInstanceState)
+    }
+
+    private fun initMapBox(savedInstanceState: Bundle?) {
         mapView = findViewById(R.id.mapView)
         mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync {
-            it.setStyle(Style.MAPBOX_STREETS, Style.OnStyleLoaded {style ->
-                // TODO: do something in here
-            })
+        val permissionListener: PermissionsListener = object : PermissionsListener {
+            override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+                /* Nothing to do in here */
+            }
+
+            override fun onPermissionResult(granted: Boolean) {
+                if (granted) {
+                    syncMapBox()
+                } else {
+                    val alertDialogInfo = AlertDialog.Builder(this@MainActivity)
+                        .setTitle(getString(R.string.info))
+                        .setCancelable(false)
+                        .setMessage(getString(R.string.permissions_denied))
+                        .setPositiveButton(getString(R.string.dismiss)) { _, _ ->
+                            finish()
+                        }
+                        .create()
+                    alertDialogInfo.show()
+                }
+            }
         }
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            syncMapBox()
+        } else {
+            permissionsManager = PermissionsManager(permissionListener)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun syncMapBox() {
+        mapView.getMapAsync { mapBoxMap ->
+            markerViewManager = MarkerViewManager(mapView, mapBoxMap)
+            this.mapBoxMap = mapBoxMap
+            this.mapBoxMap.setStyle(Style.MAPBOX_STREETS) {
+                mapBoxMap.addOnMapClickListener(this)
+                showingDeviceLocation()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun showingDeviceLocation() {
+        val locationComponent = mapBoxMap.locationComponent
+        locationComponent.activateLocationComponent(this, mapBoxMap.style!!)
+        locationComponent.isLocationComponentEnabled = true
+        locationComponent.cameraMode = CameraMode.TRACKING
+        locationComponent.renderMode = RenderMode.COMPASS
     }
 
     override fun onStart() {
@@ -50,6 +113,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        markerViewManager.onDestroy()
         mapView.onDestroy()
     }
 
@@ -57,4 +121,13 @@ class MainActivity : AppCompatActivity() {
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState!!)
     }
+
+    override fun onMapClick(point: LatLng): Boolean {
+        mapBoxMap.addMarker(
+            MarkerOptions()
+                .position(point)
+        )
+        return true
+    }
+
 }
